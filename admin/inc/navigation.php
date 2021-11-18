0c964920c95468451bfbb54f57fc2ebd53bd8f4e
akhilpgeorge <akhilpgeorge@gmail.com>
2021-11-18 22:55:05 +0530

    updates navigation

--- admin/inc/navigation.php
+++ admin/inc/navigation.php
@@ -40,8 +40,8 @@
                       </a>
                     </li>
                     
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=schedule_settings" class="nav-link nav-schedule_settings">
+                    <!-- <li class="nav-item dropdown">
+                      <a href="<?php //echo base_url ?>admin/?page=schedule_settings" class="nav-link nav-schedule_settings">
                         <i class="nav-icon fas fa-calendar-day"></i>
                         <p>
                           Schedule Settings
@@ -49,7 +49,7 @@
                       </a>
                     </li>
                     <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=user/list" class="nav-link nav-user_list">
+                      <a href="<?php //echo base_url ?>admin/?page=user/list" class="nav-link nav-user_list">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
                           User List
@@ -57,13 +57,13 @@
                       </a>
                     </li>
                     <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>admin/?page=system_info" class="nav-link nav-system_info">
+                      <a href="<?php //echo base_url ?>admin/?page=system_info" class="nav-link nav-system_info">
                         <i class="nav-icon fas fa-cogs"></i>
                         <p>
                           Settings
                         </p>
                       </a>
-                    </li>
+                    </li> -->
                   </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
--- user/inc/navigation.php
+++ user/inc/navigation.php
@@ -48,8 +48,8 @@
                         </p>
                       </a>
                     </li>
-                    <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>user/?page=user/list" class="nav-link nav-user_list">
+                    <!-- <li class="nav-item dropdown">
+                      <a href="<?php //echo base_url ?>user/?page=user/list" class="nav-link nav-user_list">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
                           User List
@@ -57,13 +57,13 @@
                       </a>
                     </li>
                     <li class="nav-item dropdown">
-                      <a href="<?php echo base_url ?>user/?page=system_info" class="nav-link nav-system_info">
+                      <a href="<?php //echo base_url ?>user/?page=system_info" class="nav-link nav-system_info">
                         <i class="nav-icon fas fa-cogs"></i>
                         <p>
                           Settings
                         </p>
                       </a>
-                    </li>
+                    </li> -->
                   </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
